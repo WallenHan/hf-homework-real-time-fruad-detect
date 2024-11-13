c75410fced77d69b7e37be09f6b23382a047b171
wallen han <hanwanli08@gmail.com>
2024-11-13 17:03:47 +0800

    初始化项目

---
+++ src/main/java/com/wallen/rtfrauddetection/RtFraudDetectionApplication.java
@@ -0,0 +1,13 @@
+package com.wallen.rtfrauddetection;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RtFraudDetectionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RtFraudDetectionApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/wallen/rtfrauddetection/RtFraudDetectionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wallen.rtfrauddetection;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RtFraudDetectionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
