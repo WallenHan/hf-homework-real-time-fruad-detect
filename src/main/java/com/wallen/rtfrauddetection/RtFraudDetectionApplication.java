d2e7f99b07023e60eae0b1bab05bedc20d24c8f1
wallen han <hanwanli08@gmail.com>
2024-11-15 18:51:17 +0800

    使用了简单加密的方案处理敏感配置信息

--- src/main/java/com/wallen/rtfrauddetection/RtFraudDetectionApplication.java
+++ src/main/java/com/wallen/rtfrauddetection/RtFraudDetectionApplication.java
@@ -1,13 +1,16 @@
 package com.wallen.rtfrauddetection;
 
+import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableEncryptableProperties
 public class RtFraudDetectionApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(RtFraudDetectionApplication.class, args);
     }
 
+    // TODO 关于金额和人员名单的清单的初始化
 }
