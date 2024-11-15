fb9167b971b8860aa7bebeb9b51b94775d1fe97a
wallen han <hanwanli08@gmail.com>
2024-11-15 18:49:02 +0800

    sqs客户端bean

---
+++ src/main/java/com/wallen/rtfrauddetection/config/AWSSQSConfig.java
@@ -0,0 +1,29 @@
+package com.wallen.rtfrauddetection.config;
+
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.regions.Regions;
+import com.amazonaws.services.sqs.AmazonSQS;
+import com.amazonaws.services.sqs.AmazonSQSClientBuilder;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AWSSQSConfig {
+    @Value("${aws.sqs.apiKey}")
+    private String accessKey;
+
+    @Value("${aws.sqs.apiSecret}")
+    private String secretKey;
+
+    @Bean
+    public AmazonSQS amazonSQSClient() {
+        BasicAWSCredentials awsCredentials = new BasicAWSCredentials(accessKey, secretKey);
+        return AmazonSQSClientBuilder.standard()
+                .withCredentials(new AWSStaticCredentialsProvider(awsCredentials))
+                .withRegion(Regions.EU_CENTRAL_1)
+                .build();
+    }
+
+}
