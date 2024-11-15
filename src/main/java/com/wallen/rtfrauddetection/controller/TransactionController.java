23b009003b93041caed3742f77781e4f7b7ad508
wallen han <hanwanli08@gmail.com>
2024-11-15 18:47:59 +0800

    提交到sqs的接口

---
+++ src/main/java/com/wallen/rtfrauddetection/controller/TransactionController.java
@@ -0,0 +1,40 @@
+package com.wallen.rtfrauddetection.controller;
+
+import com.amazonaws.services.sqs.AmazonSQS;
+import com.amazonaws.services.sqs.model.SendMessageRequest;
+import com.amazonaws.services.sqs.model.SendMessageResult;
+import com.wallen.rtfrauddetection.data.ApiResponse;
+import com.wallen.rtfrauddetection.data.Transaction;
+import com.wallen.rtfrauddetection.util.JSONUtil;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.configurationprocessor.json.JSONObject;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/v1/delivery")
+public class TransactionController {
+
+
+     private final AmazonSQS sqs;
+     @Value("${aws.sqs.queueUrl}")
+     private String queueUrl;
+
+     public TransactionController(AmazonSQS sqs) {
+          this.sqs = sqs;
+     }
+
+     @PostMapping("/transaction")
+     public ResponseEntity<ApiResponse<?>> deliveryTransaction(@RequestBody Transaction transaction) {
+
+          SendMessageRequest sendMessageRequest = new SendMessageRequest().withQueueUrl(queueUrl)
+                  .withMessageBody(JSONUtil.toJsonString(transaction))
+                  .withMessageGroupId("unchecked-transaction")
+                  .withMessageDeduplicationId(UUID.randomUUID().toString());
+          SendMessageResult result = sqs.sendMessage(sendMessageRequest);
+          return ResponseEntity.ok(ApiResponse.success(result.getSdkResponseMetadata()));
+     }
+
+}
