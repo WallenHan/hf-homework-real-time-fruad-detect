457d3da5883ae3e6891be3065253b69e708088de
wallen han <hanwanli08@gmail.com>
2024-11-18 10:03:22 +0800

    支持 aws cloudwatch 日志

--- src/main/java/com/wallen/rtfrauddetection/RtFraudDetectionApplication.java
+++ src/main/java/com/wallen/rtfrauddetection/RtFraudDetectionApplication.java
@@ -1,14 +1,19 @@
 package com.wallen.rtfrauddetection;
 
 import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
+import com.ulisesbocchio.jasyptspringboot.environment.StandardEncryptableEnvironment;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 
 @SpringBootApplication
 @EnableEncryptableProperties
 public class RtFraudDetectionApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(RtFraudDetectionApplication.class, args);
+        new SpringApplicationBuilder()
+                .environment(new StandardEncryptableEnvironment())
+                .sources(RtFraudDetectionApplication.class)
+                .run(args);
     }
 }
