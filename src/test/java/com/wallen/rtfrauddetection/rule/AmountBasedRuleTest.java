62699a2bf347058ba9a518dde53767102ee49902
wallen han <hanwanli08@gmail.com>
2024-11-18 10:03:39 +0800

    单元测试代码

--- src/test/java/com/wallen/rtfrauddetection/rule/AmountBasedRuleTest.java
+++ src/test/java/com/wallen/rtfrauddetection/rule/AmountBasedRuleTest.java
@@ -1,15 +1,34 @@
 package com.wallen.rtfrauddetection.rule;
 
+import com.wallen.rtfrauddetection.data.Transaction;
 import org.junit.jupiter.api.Test;
+
+import java.math.BigDecimal;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class AmountBasedRuleTest {
 
 
-
+    /***
+     * 以金额为准的规则，大于该阈值的都算涉嫌诈骗
+     */
     @Test
     void isFraudulent() {
-
+        Transaction transaction = Transaction.builder()
+                .amount(BigDecimal.valueOf(200000))
+                .receiver("xxxxx")
+                .sender("abcss")
+                .build();
+        AmountBasedRule rule = AmountBasedRule.builder()
+                .threshold(BigDecimal.valueOf(199999))
+                .build();
+        assertTrue(rule.isFraudulent(transaction));
+        Transaction transactionA = Transaction.builder()
+                .amount(BigDecimal.valueOf(199998))
+                .receiver("xxxxx")
+                .sender("abcss")
+                .build();
+        assertFalse(rule.isFraudulent(transaction));
     }
 }
--- src/test/java/com/wallen/rtfrauddetection/rule/SuspiciousAccountRuleTest.java
+++ src/test/java/com/wallen/rtfrauddetection/rule/SuspiciousAccountRuleTest.java
@@ -1,17 +1,50 @@
 package com.wallen.rtfrauddetection.rule;
 
+import com.wallen.rtfrauddetection.data.Transaction;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.util.Assert;
+
+import java.math.BigDecimal;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class SuspiciousAccountRuleTest {
 
-    @BeforeEach
-    void setUp() {
-    }
+
 
     @Test
     void isFraudulent() {
+        List<String> balckList = List.of("aaa", "bbb", "ccc", "ddd", "eee", "fff");
+        SuspiciousAccountRule suspiciousAccountRule = SuspiciousAccountRule.builder()
+                .suspiciousAccounts(balckList)
+                .build();
+
+        Transaction transactionA = Transaction.builder()
+                .sender("aa")
+                .receiver("bbb")
+                .amount(BigDecimal.valueOf(200))
+                .build();
+        assertTrue(suspiciousAccountRule.isFraudulent(transactionA));
+        Transaction transactionB = Transaction.builder()
+                .sender("aaa")
+                .receiver("bb")
+                .amount(BigDecimal.valueOf(200))
+                .build();
+        assertFalse(suspiciousAccountRule.isFraudulent(transactionB));
+        Transaction transactionC = Transaction.builder()
+                .sender("aaa")
+                .receiver("bbb")
+                .amount(BigDecimal.valueOf(200))
+                .build();
+        assertFalse(suspiciousAccountRule.isFraudulent(transactionC));
+        Transaction transactionD = Transaction.builder()
+                .sender("aa")
+                .receiver("bb")
+                .amount(BigDecimal.valueOf(200))
+                .build();
+        assertFalse(suspiciousAccountRule.isFraudulent(transactionD));
+
     }
 }
